de4abd647d89427b7be0b3c44bda77c906de20e8
Daniel Moya <daalmodi@gmail.com>
2025-01-06 19:35:10 -0500

    First version Back- endpoits

--- Back/users/models.py
+++ Back/users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    role = models.CharField(max_length=100)
+    loginDate = models.DateTimeField(auto_now_add=True)
+    onlineTime = models.IntegerField(default=0)
+    button1Counter = models.IntegerField(default=0)
+    button2Counter = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.name
